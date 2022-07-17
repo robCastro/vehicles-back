6a91a4313c16e551e26b7a91a4b8073cea5ef12d
Roberto Castro <recastro@presidencia.gob.sv>
2022-07-17 05:53:59 -0600

    Added procedure content as Javadoc 	modificados:     src/main/java/com/example/vehicle/repositories/VehicleRepository.java

--- src/main/java/com/example/vehicle/repositories/VehicleRepository.java
+++ src/main/java/com/example/vehicle/repositories/VehicleRepository.java
@@ -7,6 +7,18 @@ import com.example.vehicle.entities.Vehicle;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
+    /**
+     * Contenido del sp:
+     * 
+     * PROCEDURE `vehicles`.`sp_update_vehicle_count`(IN category_id bigint)
+     * 
+     *   BEGIN
+     * 
+	 *   UPDATE vehicle_category set vehicles_count = (select count(*) from vehicle v where v.category_id = category_id) where id = category_id;
+     * 
+     *  END;
+     * 
+     */
     @Procedure("sp_update_vehicle_count")
     void updateVehicleCount(Long category_id);
 }
